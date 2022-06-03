48e0587d5015b18b247204483327a3bfc45252d9
JonasFriesMediamatics <103252656+JonasFriesMediamatics@users.noreply.github.com>
2022-06-03 11:38:15 +0200

    Initial version

---
+++ apps/script.js
@@ -0,0 +1,10 @@
+function opennav() {
+    document.getElementById("hamburgernav").style.width = "50%";
+    document.getElementById("closebtn").style.display = "block";
+
+}
+function closenav() {
+    document.getElementById("hamburgernav").style.width = "0%";
+    document.getElementById("closebtn").style.display = "none";
+
+}
